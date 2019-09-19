1b51c2022baad686ae4d1f372e4db649a8681d21
FusionArc <dave@fusion-arc.uk>
2019-09-19 05:49:55 +0100

    added template views

--- core/models.py
+++ core/models.py
@@ -1,11 +1,15 @@
 from django.conf import settings
 from django.db import models
 from django.shortcuts import reverse
+from django.db.models import Sum
+from django.db.models.signals import post_save
+from django_countries.fields import CountryField
+
 
 CATEGORY_CHOICES =(
-    ('S', 'Shirt'),
-    ('SW', 'Sport Wear'),
-    ('OW', 'Outwear')
+    ('S', "Laptops/PC's"),
+    ('SW', "Consumer Electronics"),
+    ('OW', "Smart Vehicles")
 )
 
 LABEL_CHOICES =(
@@ -14,13 +18,21 @@ LABEL_CHOICES =(
     ('D', 'danger')
 )
 
+ADDRESS_CHOICES = (
+    ('B', 'Billing'),
+    ('S', 'Shipping'),
+)
+
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
-    category =models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    discount_price = models.FloatField(blank=True, null=True)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
     label = models.CharField(choices=LABEL_CHOICES, max_length=1)
     slug  = models.SlugField()
+    description = models.TextField(blank=True)
+    image = models.ImageField(blank=True)
 
     def __str__(self):
         return self.title
@@ -29,6 +41,17 @@ class Item(models.Model):
         return reverse("core:product", kwargs={
             'slug': self.slug
         })
+
+    def get_add_to_cart_url(self):
+        return reverse("core:add-to-cart", kwargs={
+            'slug': self.slug
+        })
+
+    def get_remove_from_cart_url(self):
+        return reverse("core:remove-from-cart", kwargs={
+            'slug': self.slug
+        })
+
 
 class OrderItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
